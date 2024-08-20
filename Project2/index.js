fd58f6a6268ac81790fd5d073d9fa5ff2b01a2e5
KaranKumar2002 <120900872+KaranKumar2002@users.noreply.github.com>
2024-08-20 10:41:50 +0000

    new

--- Project2/index.js
+++ Project2/index.js
@@ -1,21 +1,13 @@
-const images=['https://static1.srcdn.com/wordpress/wp-content/uploads/2023/07/iron-man-in-comic-book-armor-art.jpg'];
+const images=['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTLzwUjckGH-xDozl8qWsE-p70h3CaE0kSRLA&s','https://static1.srcdn.com/wordpress/wp-content/uploads/2023/07/iron-man-in-comic-book-armor-art.jpg'];
 const next=document.querySelector('.next');
-const previous=document.querySelector('.previous');
-let n=-1 // pointer to the next image in the images array
-let p=-1 //pointer to the previous image in the images array
-const div=document.querySelector('.main');
+let n=0 // pointer to the next image in the images array
+const image=document.querySelector('.hero');
+const body=document.querySelector('body');
 next.addEventListener('click',(e)=>{
-  const image=document.createElement('img');
   n++;
-image.src=images[n];
-if(n==images.length)
-{
-    n=-1;
-}
-console.log(n);
+  n=n%images.length; 
+  image.src=images[n];
+  body.style.background=image;
+
 })
 
-previous.addEventListener('click',(e)=>{
-   console.log('hi');
-   
-})
